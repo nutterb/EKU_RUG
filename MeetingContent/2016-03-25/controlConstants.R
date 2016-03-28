9d0c2529d98ccb14a80f505b7463ee4f928fb9cc
Benjamin <benjamin.nutter@gmail.com>
2016-03-27 21:04:39 -0400

    notes on constants

--- MeetingContent/2016-03-25/controlConstants.R
+++ MeetingContent/2016-03-25/controlConstants.R
@@ -5,3 +5,15 @@ integrate(function(w) { 1 - ptukey(w, n, Inf)}, 0, Inf)
 #* For custom written functions for calculating control constants
 #* Note: d4 isn't numerically specified.  I haven't found a source 
 #* for it yet.
+
+source("https://raw.githubusercontent.com/nutterb/Bluegrass/devel/R/constants.R")
+
+d2(1:10)
+
+d3(1:10)
+
+A2(2:10)
+
+D3(2:10)
+
+D4(2:10)
